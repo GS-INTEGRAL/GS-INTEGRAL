f49cd605ebfa51504e907dd01163f6acf36b8d90
Alberto <algalberto227@gmail.com>
2024-10-19 20:28:32 +0200

    Nuevas modificaciones plantilla de vista de prueba personalizada en la vista heredada de Helpdesk

--- custom_helpdesk_view/__manifest__.py
+++ custom_helpdesk_view/__manifest__.py
@@ -6,11 +6,13 @@
     'description': """
         Este módulo contiene campos específicos para clientes.
     """,
-    'author': 'Your Name',
+    'author': 'GS-INTEGRAL',  # Cambia esto por tu nombre real o el de tu empresa
     'depends': ['helpdesk'],
     'data': [
         'views/helpdesk_ticket_view_inherit.xml',
     ],
     'installable': True,
     'application': False,
+    'auto_install': False,  # Cambia a True si deseas que se instale automáticamente cuando se instala el módulo dependiente
 }
+
