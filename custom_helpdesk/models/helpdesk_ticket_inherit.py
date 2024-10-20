fc3e9be517a016c47197bb93c10ad10643659488
pedromayor <p27p07@hotmail.com>
2024-10-20 17:42:08 +0200

    nuevo arreglo en la vista

--- custom_helpdesk/models/helpdesk_ticket_inherit.py
+++ custom_helpdesk/models/helpdesk_ticket_inherit.py
@@ -7,6 +7,14 @@ class HelpdeskTicketInherit(models.Model):
     lugar_incidencia = fields.Char(string='Lugar de Incidencia', help='Sitio exacto dentro de la sede donde se ha producido la incidencia')
     comentario_reparacion = fields.Text(string='Comentario de Reparación', help='Comentarios positivos o negativos sobre la reparación realizada por el cliente')
     observacion_mantenimiento = fields.Text(string='Observaciones de Mantenimiento', help='Observaciones del técnico sobre dudas o problemas durante la reparación')
-
+    categoria = fields.Selection(
+        [
+            ('bricolage', 'Bricolaje'),
+            ('fontaneria', 'Fontanería'),
+            ('climatizacion', 'Climatización'),
+            ('electricidad', 'Electricidad'),
+        ],
+        string='Categoría'
+    )
     sede_imagen = fields.Binary(string='Imagen o Archivo de la Sede', attachment=True, help='Sube una imagen o archivo de la sede donde ocurrió la incidencia (PNG, JPEG, PDF)')
     lugar_incidencia_imagen = fields.Binary(string='Imagen o Archivo del Lugar de Incidencia', attachment=True, help='Sube una imagen o archivo del lugar exacto de la incidencia (PNG, JPEG, PDF)')
