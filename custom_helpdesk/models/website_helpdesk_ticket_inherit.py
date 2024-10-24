29ccf029405d9d9b551bb487a7e3557042c9a1da
pedromayor <p27p07@hotmail.com>
2024-10-24 23:45:09 +0200

    nuevos campos

--- custom_helpdesk/models/website_helpdesk_ticket_inherit.py
+++ custom_helpdesk/models/website_helpdesk_ticket_inherit.py
@@ -1,7 +1,7 @@
 from odoo import models, fields, api
 
 class HelpdeskTicketInherit(models.Model):
-    _inherit = 'website_helpdesk'
+    _inherit = 'website.helpdesk'
 
     sede = fields.Char(string='Sede', help='Centro en general donde se ha producido la incidencia')
     categoria = fields.Selection(
