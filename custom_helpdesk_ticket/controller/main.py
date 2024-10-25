f2845dc2760581864f12669fd21ba4fe4f6c7199
pedromayor <126073608+pedromayor@users.noreply.github.com>
2024-10-25 08:50:20 +0200

    modulo custom_helpdesk_ticket

--- custom_helpdesk_ticket/controller/main.py
+++ custom_helpdesk_ticket/controller/main.py
@@ -1,10 +1,11 @@
-from odoo import http
-from odoo.addons.website_helpdesk.controllers.main import WebsiteHelpdesk
+from odoo.addons.website_helpdesk.controllers.main import WebsiteForm
 
-class WebsiteHelpdeskExtended(WebsiteHelpdesk):
+class WebsiteFormExtended(WebsiteForm):
 
-    @http.route()
-    def submit_ticket(self, **kwargs):
-        if kwargs.get('sede'):
-            kwargs['description'] = f"Sede: {kwargs['sede']}\n\n{kwargs.get('description', '')}"
-        return super(WebsiteHelpdeskExtended, self).submit_ticket(**kwargs)
+    def _handle_website_form(self, model_name, **kwargs):
+        if model_name == 'helpdesk.ticket':
+            sede = kwargs.get('sede')
+            if sede:
+                kwargs['description'] = f"Sede: {sede}\n\n{kwargs.get('description', '')}"
+        
+        return super(WebsiteFormExtended, self)._handle_website_form(model_name, **kwargs)
