bb1fed36e6a26e0ef75f80fbc2a1e8f89a94a136
pedromayor <p27p07@hotmail.com>
2024-10-28 23:11:08 +0100

    paso los datos de res.partner a helpdesk.tickets

--- custom_helpdesk/controller/main.py
+++ custom_helpdesk/controller/main.py
@@ -1,21 +1,37 @@
 from odoo import http
-from odoo.http import request
 from odoo.addons.website_form.controllers.main import WebsiteForm
 
 class CustomWebsiteForm(WebsiteForm):
 
     def _handle_website_form(self, model_name, **kwargs):
-        # Ejecuta la lógica original del método padre
-        result = super(CustomWebsiteForm, self)._handle_website_form(model_name, **kwargs)
+        # Recuperar los parámetros sede y lugar
+        sede = request.params.get('sede')
+        lugar = request.params.get('lugar')
+        email = request.params.get('partner_email')
 
-        # Añadir lógica solo si es un ticket de soporte
-        if model_name == 'helpdesk.ticket':
-            partner_id = request.params.get('partner_id')
-            partner = request.env['res.partner'].sudo().browse(partner_id)
+        if email:
+            if request.env.user.email == email:
+                partner = request.env.user.partner_id
+            else:
+                partner = request.env['res.partner'].sudo().search([('email', '=', email)], limit=1)
+            
+            # Si no existe el partner, crearlo con los campos adicionales
+            if not partner:
+                partner = request.env['res.partner'].sudo().create({
+                    'email': email,
+                    'name': request.params.get('partner_name', False),
+                    'lang': request.lang.code,
+                    'sede': sede,
+                    'lugar': lugar,
+                })
+            else:
+                # Si el partner existe, actualizar los campos sede y lugar
+                partner.sudo().write({
+                    'sede': sede,
+                    'lugar': lugar,
+                })
 
-            # Asignar los campos adicionales `sede` y `lugar` al ticket
-            if partner:
-                request.params['sede'] = partner.sede
-                request.params['lugar'] = partner.lugar
+            request.params['partner_id'] = partner.id
 
-        return result
+        # Llamar al método original
+        return super(CustomWebsiteForm, self)._handle_website_form(model_name, **kwargs)
