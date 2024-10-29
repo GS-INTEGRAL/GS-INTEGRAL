609042e3cad9ddb35234a7a3110481bccf2d9a42
pedromayor <p27p07@hotmail.com>
2024-10-29 18:36:30 +0100

    nueva prueba

--- custom_helpdesk/controller/main.py
+++ custom_helpdesk/controller/main.py
@@ -6,14 +6,19 @@ class CustomWebsiteHelpdesk(WebsiteHelpdesk):
 
     @http.route(['/helpdesk/create'], type='http', auth="public", website=True, csrf=False)
     def helpdesk_ticket_create(self, **kwargs):
-        # Extraer el usuario autenticado y sus datos de res.partner
+        # Obtenemos el usuario autenticado y sus datos de `res.partner`
         user = request.env.user
         if user.partner_id:
+            # Pasamos los valores de `sede` y `lugar` desde el perfil del usuario al contexto
             kwargs['sede'] = user.partner_id.sede
             kwargs['lugar'] = user.partner_id.lugar
 
-        # Llamada al controlador original para crear el ticket
-        return super(CustomWebsiteHelpdesk, self).helpdesk_ticket_create(**kwargs)
-
+        # Llamada al controlador original para renderizar el formulario
+        response = super(CustomWebsiteHelpdesk, self).helpdesk_ticket_create(**kwargs)
+        response.qcontext.update({
+            'sede': kwargs.get('sede'),
+            'lugar': kwargs.get('lugar')
+        })
+        return response
 
 
