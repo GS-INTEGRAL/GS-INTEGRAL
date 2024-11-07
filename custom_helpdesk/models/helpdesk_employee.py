7b646b0747e9799ec1a7055e509e9ba990942490
pedromayor <126073608+pedromayor@users.noreply.github.com>
2024-11-07 16:46:10 +0100

    arreglando extraccion de datos

--- custom_helpdesk/models/helpdesk_employee.py
+++ custom_helpdesk/models/helpdesk_employee.py
@@ -18,40 +18,37 @@ class HelpdeskEmployee(models.Model):
     product_qty = fields.Float(
         string="Cantidad", default=1.0, help="Cantidad de producto requerida"
     )
-    attachment = fields.Image(
+    attachment = fields.Binary(
         string="Imagen del Producto",
         help="Adjunta una imagen del producto necesario",
     )
+    attachment_filename = fields.Char("Nombre de Archivo de la Imagen")
 
-    # Botón para crear una orden de compra
     @api.model
     def create_purchase_order_line(self):
-        # Verificar que se ha seleccionado un producto y una cantidad válida
         if not self.product_id or self.product_qty <= 0:
             raise UserError(
                 _("Debe especificar un producto y una cantidad válida para la compra.")
             )
 
-        # Crear la orden de compra o seleccionar una existente
         purchase_order = self.env["purchase.order"].create(
             {
-                "partner_id": self.env.user.company_id.partner_id.id,  
+                "partner_id": self.env.user.company_id.partner_id.id,
                 "origin": self.name or _("Ticket de Helpdesk: %s") % self.id,
             }
         )
 
-        # Crear una línea en la orden de compra
         self.env["purchase.order.line"].create(
             {
                 "order_id": purchase_order.id,
                 "product_id": self.product_id.id,
                 "name": self.name or self.product_id.name,
                 "product_qty": self.product_qty,
+                "price_unit": self.product_id.standard_price,
                 "date_planned": fields.Date.today(),
             }
         )
 
-        # Confirmar la orden de compra si es necesario
         purchase_order.button_confirm()
         return {
             "type": "ir.actions.act_window",
