f75562e8645b4364b3f88f75afc98c5f0a0b15b3
pedromayor <126073608+pedromayor@users.noreply.github.com>
2024-11-13 16:29:48 +0100

    actualizo modulo

--- custom_auth_sing_in/__manifest__.py
+++ custom_auth_sing_in/__manifest__.py
@@ -8,8 +8,8 @@
     "license": "LGPL-3",
     "depends": ["base", "auth_signup", "website_helpdesk"],
     "data": [
-        "views/auth_signup_login_inherit.xml",
-        "views/view_partners_form_inherited.xml",
+        # "views/auth_signup_login_inherit.xml",
+        # "views/view_partners_form_inherited.xml",
     ],
     "installable": True,
     "application": False,
