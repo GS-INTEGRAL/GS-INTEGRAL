8dc4e453371019d264bb4f4e4f0d36c32021ec72
pedromayor <126073608+pedromayor@users.noreply.github.com>
2024-11-13 15:25:30 +0100

    actualizo modulo

--- custom_estancia_capitulo/models/obra.py
+++ custom_estancia_capitulo/models/obra.py
@@ -5,5 +5,6 @@ class Obra(models.Model):
     _name = "obra"
     _description = "Obra"
 
-    name = fields.Char(string="Nombre de la Obra", required=True)
+    name = fields.Char(string="Nombre de la Obra")
     estancias_ids = fields.One2many("estancias.capitulo", "obra_id", string="Estancias")
+
