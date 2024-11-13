a5aa03a6d8e7de54fb8f5629c4e57888e10ce356
pedromayor <126073608+pedromayor@users.noreply.github.com>
2024-11-13 16:22:08 +0100

    actualizo modulo

--- custom_auth_sing_in/models/res_partner.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class ResPartner(models.Model):
-    _inherit = "res.partner"
-
-    obra_id = fields.Char(string="Sede/Obra")
-    estancia_id = fields.Char(string="Estancia/Capitulo")
--- custom_helpdesk/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import helpdesk_ticket_inherit
-from . import helpdesk_employee
