6dfea2cbb9bd0a476f30010ae2ed6422bc821ea9
pedromayor <p27p07@hotmail.com>
2024-11-14 21:13:48 +0100

    ajusto controlador

--- custom_helpdesk/controller/main.py
+++ custom_helpdesk/controller/main.py
@@ -9,27 +9,29 @@ _logger = logging.getLogger(__name__)
 class CustomWebsiteHelpdesk(WebsiteHelpdesk):
 
     @http.route(
-        "/website/helpdesk/form/",
+        "/website/helpdesk/create",
         type="http",
         auth="public",
         website=True,
         csrf=False,
     )
-    def website_form(self, **kwargs):
+    def website_create(self, **kwargs):
         user = request.env.user
         partner = user.partner_id if user.partner_id else None
 
         # Obtener la categoría desde los argumentos del formulario
         categoria = kwargs.get("categoria")
-        
+
         # Asignar valores para obra y estancia si el partner está disponible
         if partner:
             kwargs["obra_id"] = partner.obra_id.display_name if partner.obra_id else ""
-            kwargs["estancia_id"] = partner.estancia_id.display_name if partner.estancia_id else ""
+            kwargs["estancia_id"] = (
+                partner.estancia_id.display_name if partner.estancia_id else ""
+            )
         else:
             kwargs["obra_id"] = ""
             kwargs["estancia_id"] = ""
-        
+
         # Preparar los valores para el ticket
         ticket_values = {
             "categoria": categoria,
@@ -37,7 +39,7 @@ class CustomWebsiteHelpdesk(WebsiteHelpdesk):
             "obra_id": kwargs["obra_id"],
             "estancia_id": kwargs["estancia_id"],
         }
-        
+
         # Crear el ticket en el modelo de helpdesk
         ticket = request.env["helpdesk.ticket"].sudo().create(ticket_values)
 
