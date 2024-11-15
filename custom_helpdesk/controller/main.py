56c7e9060e59c31331ace28a9c658069007c5448
pedromayor <p27p07@hotmail.com>
2024-11-15 21:44:59 +0100

    ajustes

--- custom_helpdesk/controller/main.py
+++ custom_helpdesk/controller/main.py
@@ -19,39 +19,18 @@ class CustomWebsiteHelpdesk(WebsiteHelpdesk):
         user = request.env.user
         partner = user.partner_id if user.partner_id else None
 
-        # # Obtener la categoría desde los argumentos del formulario
-        # categoria = kwargs.get("categoria")
-
-        # Asignar valores para obra y estancia si el partner está disponible
         if partner:
-
-            kwargs["obra_id"] = partner.obra_id.display_name if partner.obra_id else ""
-            kwargs["estancia_id"] = (
-                partner.estancia_id.display_name if partner.estancia_id else ""
-            )
+            obra_id = partner.obra_id.display_name if partner.obra_id else ""
+            estancia_id = partner.estancia_id.display_name if partner.estancia_id else ""
         else:
-            
-            kwargs["obra_id"] = ""
-            kwargs["estancia_id"] = ""
+            obra_id = ""
+            estancia_id = ""
 
-        # Preparar los valores para el ticket
-        ticket_values = {
-            # "categoria": categoria,
-            "partner_id": partner.id if partner else None,
-            "obra_id": kwargs["obra_id"],
-            "estancia_id": kwargs["estancia_id"],
-        }
-
-        # Crear el ticket en el modelo de helpdesk
-        ticket = request.env["helpdesk.ticket"].sudo().create(ticket_values)
-
-        # Renderizar la vista con los datos pasados
         return request.render(
             "website_helpdesk.team_form_1",
             {
                 "partner": partner,
-                "obra_id": kwargs["obra_id"],
-                "estancia_id": kwargs["estancia_id"],
-                "ticket": ticket,
+                "obra_id": obra_id,
+                "estancia_id": estancia_id,
             },
         )
