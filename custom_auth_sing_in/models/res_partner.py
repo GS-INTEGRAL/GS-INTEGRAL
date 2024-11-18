bfba0233dfd6fa08b27231b8b50366fa1541f853
pedromayor <p27p07@hotmail.com>
2024-11-18 21:06:00 +0100

    hago cambios

--- custom_auth_sing_in/models/res_partner.py
+++ custom_auth_sing_in/models/res_partner.py
@@ -22,7 +22,7 @@ class ResPartner(models.Model):
     @api.onchange("obra_id")
     def _onchange_obra(self):
         """Actualizar las opciones de estancia en función de la obra seleccionada."""
-        if self.obra_id == "maristas":
+        if self.obra_id == "Maristas":
             self.estancia_id = False
             return {"domain": {"estancia_id": [("id", "in", ["fuensanta", "merced"])]}}
         else:
@@ -34,11 +34,11 @@ class ResPartner(models.Model):
 
         estancias = []
 
-        if self.obra_id and self.obra_id.name == "Maristas":
+        if self.obra_id and self.obra_id.name == "maristas":
 
             obra_secundaria = self.obra_id.obra_secundaria
 
-            if obra_secundaria == "Fuensanta":
+            if obra_secundaria == "fuensanta":
                 estancias = [
                     "Infantil 2 años - A",
                     "1º Infantil 3 años - A",
@@ -95,49 +95,43 @@ class ResPartner(models.Model):
                     "Laboratorio",
                     "Danza",
                 ]
-        elif obra_secundaria == "Merced":
-            estancias = [
-                "1º ESO - A",
-                "1º ESO - B",
-                "1º ESO - C",
-                "1º ESO - D",
-                "1º ESO - E",
-                "1º ESO - F",
-                "2º ESO - A",
-                "2º ESO - B",
-                "2º ESO - C",
-                "2º ESO - D",
-                "2º ESO - E",
-                "2º ESO - F",
-                "3º ESO - A",
-                "3º ESO - B",
-                "3º ESO - C",
-                "3º ESO - D",
-                "3º ESO - E",
-                "3º ESO - F",
-                "4º ESO - A",
-                "4º ESO - B",
-                "4º ESO - C",
-                "4º ESO - D",
-                "4º ESO - E",
-                "4º ESO - F",
-                "1º Bachillerato - A",
-                "1º Bachillerato - B",
-                "1º Bachillerato - C",
-                "1º Bachillerato - D",
-                "1º Bachillerato - E",
-                "2º Bachillerato - A",
-                "2º Bachillerato - B",
-                "2º Bachillerato - C",
-                "2º Bachillerato - D",
-                "2º Bachillerato - E",
-            ]
-
-        estancia_records = self.env["custom.estancia"].search(
-            [("name", "in", estancias)]
-        )
+            elif obra_secundaria == "merced":
+                estancias = [
+                    "1º ESO - A",
+                    "1º ESO - B",
+                    "1º ESO - C",
+                    "1º ESO - D",
+                    "1º ESO - E",
+                    "1º ESO - F",
+                    "2º ESO - A",
+                    "2º ESO - B",
+                    "2º ESO - C",
+                    "2º ESO - D",
+                    "2º ESO - E",
+                    "2º ESO - F",
+                    "3º ESO - A",
+                    "3º ESO - B",
+                    "3º ESO - C",
+                    "3º ESO - D",
+                    "3º ESO - E",
+                    "3º ESO - F",
+                    "4º ESO - A",
+                    "4º ESO - B",
+                    "4º ESO - C",
+                    "4º ESO - D",
+                    "4º ESO - E",
+                    "4º ESO - F",
+                    "1º Bachillerato - A",
+                    "1º Bachillerato - B",
+                    "1º Bachillerato - C",
+                    "1º Bachillerato - D",
+                    "1º Bachillerato - E",
+                    "2º Bachillerato - A",
+                    "2º Bachillerato - B",
+                    "2º Bachillerato - C",
+                    "2º Bachillerato - D",
+                    "2º Bachillerato - E",
+                ]
 
         self.estancia_id = False
-        return {"domain": {"estancia_id": [("id", "in", estancia_records.ids)]
-                           }
-                }
+        return {"domain": {"estancia_id": estancias}}
