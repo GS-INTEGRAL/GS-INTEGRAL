26b882534b3937e1f90c5fd1dacf5c9459dd8d83
pedromayor <p27p07@hotmail.com>
2024-11-20 22:20:12 +0100

    login

--- custom_helpdesk/controller/main.py
+++ custom_helpdesk/controller/main.py
@@ -6,37 +6,42 @@ import logging
 
 _logger = logging.getLogger(__name__)
 
-
 class CustomWebsiteHelpdesk(WebsiteHelpdesk):
 
     @http.route(
-        ["/helpdesk/create"],
+        ["/custom_helpdesk/create"],
         type="http",
         auth="public",
         website=True,
         csrf=False,
     )
-    def helpdesk_ticket_create(self, **kwargs):
+    def website_create(self, **kwargs):
+        # Verificar si el usuario está autenticado
+        if not request.env.user or request.env.user._is_public():
+            return request.redirect("/web/login?redirect=/helpdesk")
+
+        # Obtener datos del usuario
         user = request.env.user
         partner = user.partner_id if user.partner_id else None
+        obra_id = partner.obra_id
+        estancia_id = partner.estancia_id
+        categoria = kwargs.get("categoria")
 
-        if partner:
+        # Limpiar descripción
+        raw_description = kwargs.get("description", "")
+        clean_description = html_sanitize(raw_description)
 
-            kwargs["obra_id"] = partner.obra_id
-            kwargs["estancia_id"] = partner.estancia_id
-        else:
+        # Crear ticket
+        ticket_vals = {
+            "name": "Ticket desde la Web",
+            "partner_id": partner.id,
+            "obra_id": obra_id,
+            "estancia_id": estancia_id,
+            "categoria": categoria,
+            "description": clean_description,
+        }
 
-            kwargs["obra_id"] = ""
-            kwargs["estancia_id"] = ""
+        ticket = request.env["helpdesk.ticket"].sudo().create(ticket_vals)
 
-        # raw_description = kwargs.get("description", "")
-        # clean_description = html_sanitize(raw_description)
-
-        return request.render(
-            "website_helpdesk.team_form_1",
-            {
-                "partner": partner,
-                "obra_id": kwargs.get("obra_id"),
-                "estancia_id": kwargs.get("estancia_id"),
-            },
-        )
+        # Redirigir al ticket creado
+        return request.redirect(f"/helpdesk/ticket/{ticket.id}")
