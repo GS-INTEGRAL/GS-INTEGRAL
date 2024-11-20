00613c42cd9d5773afa1d60c2b093f8a6bc3b086
pedromayor <p27p07@hotmail.com>
2024-11-20 21:40:10 +0100

    login

--- custom_helpdesk/controller/main.py
+++ custom_helpdesk/controller/main.py
@@ -2,6 +2,7 @@ from odoo import http
 from odoo.http import request
 from odoo.addons.website_helpdesk.controllers.main import WebsiteHelpdesk
 from lxml.html.clean import Cleaner
+from odoo.tools import html_sanitize
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -17,8 +18,7 @@ class CustomWebsiteHelpdesk(WebsiteHelpdesk):
         csrf=False,
     )
     def website_create(self, **kwargs):
-        if not request.env.user.id or request.env.user.has_group("base.group_public"):
-
+        if not request.env.user or request.env.user._is_public():
             return request.redirect("/web/login?redirect=/helpdesk")
 
         user = request.env.user
@@ -29,17 +29,8 @@ class CustomWebsiteHelpdesk(WebsiteHelpdesk):
         categoria = kwargs.get("categoria")
 
         raw_description = kwargs.get("description", "")
-        cleaner = Cleaner(
-            scripts=False,
-            javascript=True,
-            style=True,
-            links=False,
-            remove_unknown_tags=False,
-            safe_attrs_only=True,
-            safe_attrs=frozenset(["href", "title", "alt", "src"]),
-        )
-        clean_description = cleaner.clean_html(raw_description)
-
+        clean_description = html_sanitize(raw_description)
+        
         ticket_vals = {
             "name": "Ticket desde la Web",
             "partner_id": partner.id,
