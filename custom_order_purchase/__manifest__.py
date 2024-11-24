9d8e9cae872d95bc6c4759c7e94c743dc782a4c0
pedromayor <p27p07@hotmail.com>
2024-11-24 17:28:49 +0100

    ajusto modulo

--- custom_order_purchase/__manifest__.py
+++ custom_order_purchase/__manifest__.py
@@ -1,22 +1,26 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Custom_order_purchase',
-    'version': '17.0.1.0.0',
-    'summary': """ Custom_order_purchase Summary """,
-    'author': 'Pedro Mayor',
-    'website': '',
-    'category': 'Implements',
-    'depends': ['base', 'web', 'helpdesk', 'purchase'],
-    "data": [
-            "views/view_helpdesk_ticket_form_inherit_purchase.xml",
+    "name": "Custom_order_purchase",
+    "version": "17.0.1.0.0",
+    "summary": """ Custom_order_purchase Summary """,
+    "author": "Pedro Mayor",
+    "website": "",
+    "category": "Implements",
+    "depends": [
+        "base",
+        "web",
+        "helpdesk",
+        "purchase",
+        "product",
     ],
-    'assets': {
-              'web.assets_backend': [
-                  'custom_order_purchase/static/src/**/*'
-              ],
-          },
-    'application': True,
-    'installable': True,
-    'auto_install': False,
-    'license': 'LGPL-3',
+    "data": [
+        "views/view_helpdesk_ticket_form_inherit_purchase.xml",
+    ],
+    "assets": {
+        "web.assets_backend": ["custom_order_purchase/static/src/**/*"],
+    },
+    "application": True,
+    "installable": True,
+    "auto_install": False,
+    "license": "LGPL-3",
 }
