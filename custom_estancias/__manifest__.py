d27400e6d8cc50c45841431e8fbdb6266121ec98
pedromayor <126073608+pedromayor@users.noreply.github.com>
2024-11-29 09:28:02 +0100

    nuevo modulo

---
+++ custom_estancias/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Custom_estancias',
+    'version': '',
+    'summary': """ Custom_estancias Summary """,
+    'author': '',
+    'website': '',
+    'category': '',
+    'depends': ['base', ],
+    "data": [
+        "views/sedes_estancias_views.xml",
+        "security/ir.model.access.csv",
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
