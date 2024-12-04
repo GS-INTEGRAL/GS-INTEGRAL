a6c0a1519d6179a7ed5954c2083e35edcfc2f27f
pedromayor <p27p07@hotmail.com>
2024-12-04 22:26:32 +0100

    compras 12

--- custom_order_purchase/models/purchase_order.py
+++ custom_order_purchase/models/purchase_order.py
@@ -5,7 +5,7 @@ class CustomPurchaseOrderLine(models.Model):
     _name = "custom.purchase.order.line"
     _description = "Custom Purchase Order Line"
 
-    name = fields.Text(string="Description", required=True)
+    name = fields.Text(string="Description", required=False)
     product_id = fields.Many2one("product.product", string="Producto")
     product_qty = fields.Float(string="Cantidad", required=True)
     ticket_id = fields.Many2one("helpdesk.ticket", string="Ticket relacionado")
