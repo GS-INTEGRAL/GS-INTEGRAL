2eae0e4295458fa0e98c7961a9d8e0b8ce8a250a
pedromayor <p27p07@hotmail.com>
2024-12-04 21:36:39 +0100

    compras

--- custom_order_purchase/models/helpdesk_purchase_line.py
+++ custom_order_purchase/models/helpdesk_purchase_line.py
@@ -9,3 +9,15 @@ class HelpdeskTicket(models.Model):
         "ticket_id",
         string="Líneas de compra personalizadas",
     )
+
+    @api.depends("custom_purchase_order_ids.product_id")
+    def _compute_product_ids(self):
+        for ticket in self:
+            ticket.product_ids = ticket.custom_purchase_order_ids.mapped("product_id")
+
+    product_ids = fields.Many2many(
+        "product.product",
+        string="Productos",
+        compute="_compute_product_ids",
+        store=True,
+    )
