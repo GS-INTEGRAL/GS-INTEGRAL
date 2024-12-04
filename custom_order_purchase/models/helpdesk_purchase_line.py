daac41d75da8e1eed79c5f1e62900c9f89046640
pedromayor <p27p07@hotmail.com>
2024-12-04 21:07:01 +0100

    compras

--- custom_order_purchase/models/helpdesk_purchase_line.py
+++ custom_order_purchase/models/helpdesk_purchase_line.py
@@ -4,6 +4,4 @@ from odoo import models, fields
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    purchase_order_ids = fields.One2many(
-        "purchase.order.line", "ticket_id", string="Líneas de compra"
-    )
+    purchase_order_ids = fields.One2many("purchase.order.line", "ticket_id", string="Líneas de compra")
--- custom_order_purchase/models/purchase_order.py
+++ custom_order_purchase/models/purchase_order.py
@@ -4,24 +4,24 @@ from odoo import models, fields
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
+    name = fields.Text(string='Description', required=True)
     product_id = fields.Many2one('product.product', string="Producto")
     product_qty = fields.Float(string="Cantidad", required=True)
-    price_unit = fields.Float(string="Precio Unitario", required=True)
     ticket_id = fields.Many2one("helpdesk.ticket", string="Ticket relacionado")
 
-    def action_create_purchase_order(self):
-        purchase_order = self.env['purchase.order'].create({
-            'partner_id': self.partner_id.id,
-            'order_line': [(0, 0, {
-                'product_id': line.product_id.id,
-                'product_qty': line.product_qty,
-                'price_unit': line.price_unit,
-                'taxes_id': [(6, 0, line.taxes_id.ids)],
-            }) for line in self.purchase_order_ids],
-        })
-        return {
-            'type': 'ir.actions.act_window',
-            'res_model': 'purchase.order',
-            'view_mode': 'form',
-            'res_id': purchase_order.id,
-        }
+    # def action_create_purchase_order(self):
+    #     purchase_order = self.env['purchase.order'].create({
+    #         'partner_id': self.partner_id.id,
+    #         'order_line': [(0, 0, {
+    #             'product_id': line.product_id.id,
+    #             'product_qty': line.product_qty,
+    #             'price_unit': line.price_unit,
+    #             'taxes_id': [(6, 0, line.taxes_id.ids)],
+    #         }) for line in self.purchase_order_ids],
+    #     })
+    #     return {
+    #         'type': 'ir.actions.act_window',
+    #         'res_model': 'purchase.order',
+    #         'view_mode': 'form',
+    #         'res_id': purchase_order.id,
+    #     }
