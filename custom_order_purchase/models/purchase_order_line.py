9d50494e440a23599a603d29fab03f34720db3dd
pedromayor <p27p07@hotmail.com>
2024-12-07 19:03:00 +0100

    modulo de compras

--- custom_order_purchase/models/purchase_order_line.py
+++ custom_order_purchase/models/purchase_order_line.py
@@ -3,4 +3,5 @@ from odoo import _, models, fields, api
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order'
 
-    helpdesk_user_id = fields.Many2one('res.users', string='Empleado Asignado', related='order_id.helpdesk_ticket_id.user_id', store=True, readonly=True)
+    helpdesk_ticket_id = fields.Many2one('helpdesk.ticket', string='Ticket Relacionado')
+    helpdesk_user_id = fields.Many2one('res.users', string='Empleado Asignado', related='helpdesk_ticket_id.user_id', store=True, readonly=True)
