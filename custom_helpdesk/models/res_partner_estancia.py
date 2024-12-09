b5dfa443ef7cacb5bbd274fbe54051366941992b
pedromayor <p27p07@hotmail.com>
2024-12-09 19:41:19 +0100

    controlador

--- custom_helpdesk/models/res_partner_estancia.py
+++ custom_helpdesk/models/res_partner_estancia.py
@@ -1,12 +1,6 @@
 from odoo import models, fields
 
-class PartnerEstancia(models.Model):
-    _name = "res.partner.estancia_id"
-    _order = "name"
-    _description = "Partner Estancia"
+class ResPartnerEstancia(models.Model):
+    _inherit = 'res.partner.estancia_id'
 
-    estancia_id = fields.Many2one(
-        'res.partner.estancia_id', 
-        string="Estancia/Capítulo"
-    )
     is_estancia = fields.Boolean(default=True)
--- custom_helpdesk/models/res_partner_obra.py
+++ custom_helpdesk/models/res_partner_obra.py
@@ -1,12 +1,6 @@
 from odoo import models, fields
 
-class PartnerObraSecundaria(models.Model):
-    _name = "res.partner.obra_secundaria"
-    _order = "name"
-    _description = "Partner Obra Secundaria"
+class ResPartnerObraSecundaria(models.Model):
+    _inherit = 'res.partner.obra_secundaria'
 
-    obra_secundaria = fields.Many2one(
-        'res.partner.obra_secundaria', 
-        string="Obra/sede"
-    )
     is_obra_secundaria = fields.Boolean(default=True)
