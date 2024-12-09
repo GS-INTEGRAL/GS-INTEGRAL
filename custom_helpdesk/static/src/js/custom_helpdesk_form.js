5dfc4c4d68492051cda1edd467e55306ed7fca31
pedromayor <p27p07@hotmail.com>
2024-12-09 12:16:23 +0100

    nuevo ajuste

--- custom_helpdesk/static/src/js/custom_helpdesk_form.js
+++ custom_helpdesk/static/src/js/custom_helpdesk_form.js
@@ -3,25 +3,13 @@
 import { _t } from "@web/core/l10n/translation";
 import FormEditorRegistry from "@website/js/form_editor_registry";
 
+const existingCreateTicket = FormEditorRegistry.get('create_ticket');
+
 FormEditorRegistry.add('create_ticket', {
-    formFields: [{
-        type: 'char',
-        required: true,
-        name: 'partner_name',
-        fillWith: 'name',
-        string: _t('Your Name'),
-    }, {
-        type: 'email',
-        required: true,
-        name: 'partner_email',
-        fillWith: 'email',
-        string: _t('Your Email'),
-    }, {
-        type: 'char',
-        modelRequired: true,
-        name: 'name',
-        string: _t('Subject'),
-    }, {
+    ...existingCreateTicket,
+    formFields: [
+        ...existingCreateTicket.formFields,
+        {
         type: 'char',
         name: 'obra_secundaria',
         string: _t('Sede/Obra'),
@@ -48,20 +36,6 @@ FormEditorRegistry.add('create_ticket', {
             ['carpinteria', 'Carpintería'],
             ['cristaleria', 'Cristalería'],
         ],
-    }, {
-        type: 'text',
-        name: 'description',
-        string: _t('Description'),
-    }, {
-        type: 'binary',
-        custom: true,
-        name: _t('Attachment'),
-    }],
-    fields: [{
-        name: 'team_id',
-        type: 'many2one',
-        relation: 'helpdesk.team',
-        string: _t('Helpdesk Team'),
-    }],
-    successPage: '/your-ticket-has-been-submitted',
+    }, 
+],    
 });
