1eea846b5124291815f5e46feb3eedbad686ed7a
pedromayor <p27p07@hotmail.com>
2024-12-09 22:16:44 +0100

    controlador

--- custom_helpdesk/models/helpdesk_ticket_inherit.py
+++ custom_helpdesk/models/helpdesk_ticket_inherit.py
@@ -3,7 +3,7 @@ from odoo.exceptions import UserError
 
 
 class HelpdeskTicketInherit(models.Model):
-    _inherit ="helpdesk.ticket"
+    _inherit =["helpdesk.ticket", "website_helpdesk"]
 
     images = fields.Many2many(
         "ir.attachment",
