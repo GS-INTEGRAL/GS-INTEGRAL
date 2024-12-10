3ea352e03c41222488347bf9df18e4d800413db9
pedromayor <p27p07@hotmail.com>
2024-12-10 19:34:22 +0100

    nueva modificacion

--- custom_helpdesk/controller/main.py
+++ custom_helpdesk/controller/main.py
@@ -15,36 +15,36 @@ def ensure_authenticated_user():
     return None
 
 
-# class CustomWebsiteHelpdeskTeams(http.Controller):
+class CustomWebsiteHelpdeskTeams(http.Controller):
 
-#     @http.route(
-#         ["/helpdesk", '/helpdesk/<model("helpdesk.team"):team>'],
-#         type="http",
-#         auth="user",
-#         website=True,
-#     )
-#     def website_helpdesk_teams(self, team=None, **kwargs):
-#         if request.env.user._is_public():
+    @http.route(
+        ["/helpdesk", '/helpdesk/<model("helpdesk.team"):team>'],
+        type="http",
+        auth="user",
+        website=True,
+    )
+    def website_helpdesk_teams(self, team=None, **kwargs):
+        if request.env.user._is_public():
 
-#             return request.redirect("/web/login?redirect=/helpdesk")
+            return request.redirect("/web/login?redirect=/helpdesk")
 
-#         teams_domain = [("use_website_helpdesk_form", "=", True)]
-#         if not request.env.user.has_group("helpdesk.group_helpdesk_manager"):
-#             if team and not team.is_published:
-#                 raise NotFound()
-#             teams_domain.append(("website_published", "=", True))
+        teams_domain = [("use_website_helpdesk_form", "=", True)]
+        if not request.env.user.has_group("helpdesk.group_helpdesk_manager"):
+            if team and not team.is_published:
+                raise NotFound()
+            teams_domain.append(("website_published", "=", True))
 
-#         teams = request.env["helpdesk.team"].search(teams_domain, order="id asc")
-#         if not teams:
-#             raise NotFound()
+        teams = request.env["helpdesk.team"].search(teams_domain, order="id asc")
+        if not teams:
+            raise NotFound()
 
-#         # Renderizamos la vista del equipo de helpdesk
-#         result = {
-#             "team": team or teams[0],
-#             "multiple_teams": len(teams) > 1,
-#             "main_object": team or teams[0],
-#         }
-#         return request.render("website_helpdesk.team", result)
+        # Renderizamos la vista del equipo de helpdesk
+        result = {
+            "team": team or teams[0],
+            "multiple_teams": len(teams) > 1,
+            "main_object": team or teams[0],
+        }
+        return request.render("website_helpdesk.team", result)
 
 
 class CustomWebsiteHelpdesk(WebsiteHelpdesk):
