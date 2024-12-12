a82473bd9c82c5e827dd8bb136e46941cc894750
pedromayor <126073608+pedromayor@users.noreply.github.com>
2024-12-12 08:40:37 +0100

    ajustes en custom_helpdesk

--- custom_helpdesk/__manifest__.py
+++ custom_helpdesk/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Custom Helpdesk Extension",
-    "version": "17.0.1.0",
+    "version": "17.0.1.1",
     "summary": "Extensión del módulo de Helpdesk con campos personalizados",
     "author": "Pedro Mayor",
     "category": "Helpdesk",
